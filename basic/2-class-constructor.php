80b7ea0e6cf230af918c1be3b471e917bfc8e6bd
Muhammad Sujan <sujan@arogga.net>
2023-08-07 18:05:26 +0600

    PHP 8 Constructor with data

---
+++ basic/2-class-constructor.php
@@ -0,0 +1,32 @@
+<?php
+class Employee {
+
+    public float $salary;
+    public $designation;
+    public $joinDate;
+
+    public function __construct( float $salary, string $joinDate, string $designation) {
+        $this->salary = $salary ;
+        $this->joinDate = $joinDate ;
+        $this->designation = $designation ;
+    }
+    public function getEmployeeSalary () {
+        return $this->salary ;
+    }
+}
+
+$emp = new Employee('30000.00', '01/01/2023', 'Senior Software Engineer');
+
+echo "<pre>";
+print_r($emp->getEmployeeSalary());
+echo "</pre>";
+
+
+// Employee Object
+// (
+//     [salary] => 2342
+//     [joinDate] => 01/01/2011
+//     [designation] => Engineer
+// )
+
+?>
