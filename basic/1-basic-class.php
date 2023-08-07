46ed1c9865d7995c1d74dec4beb732c305fb5b50
Muhammad Sujan <sujan@arogga.net>
2023-08-07 17:43:12 +0600

    PHP 8 Basic Class

---
+++ basic/1-basic-class.php
@@ -0,0 +1,16 @@
+<?php
+class Employee {
+    public float $salary;
+    public $joinDate;
+    public $designation;
+}
+
+$emp = new Employee;
+$emp->salary = 2342;
+$emp->joinDate ='01/01/2011';
+$emp->designation ='Engineer';
+echo "<pre>";
+print_r($emp);
+echo "</pre>";
+
+?>
